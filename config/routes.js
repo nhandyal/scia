4631d59d3c0a124499ee0eda5568d61a50047e98
Nikhil Handyal <nhandyal@gmail.com>
2013-09-06 04:52:30 -0700

    Abort finish, begin emergency hack for GM

--- config/routes.js
+++ config/routes.js
@@ -1,25 +1,38 @@
 module.exports = function(app){
 
+
 	app.get("/", function(req, res){
-		res.render("index");
+		res.render("indexMain");
 	});
 
-	app.get("/addUser", function(req, res){
+
+	app.post("/addUser", function(req, res){
+		console.log(req);
+		console.log(req.body.firstName);
+		console.log(req.body.lastName);
+		console.log(req.body.email);
+		console.log(req.body.phonenumber);
+		console.log(req.body.major);
+		console.log(req.body.year);
+		res.send("lol");
+
+		/*
 		var name = req.query.user,
-						email = req.query.email;
+			email = req.query.email;
 
 		var user = new users({
-						name: name,
-						email: email
+			name: name,
+			email: email
 		});
 
 		user.save(function(err, result){
-						if(err){
-										res.send("something bad happend");
-						}
-						else{
-										res.send("saved new user");
-						}
+			if(err){
+				res.send("something bad happend");
+			}
+			else{
+				res.send("saved new user");
+			}
 		});
+*/
 	});
 }
