2441f060e258d8e960938458d0a3bd1fb425fc53
Nikhil Handyal <nhandyal@gmail.com>
2013-09-10 13:08:12 -0700

    Render accurate

--- public/js/dist/SCIA.js
+++ public/js/dist/SCIA.js
@@ -11,29 +11,55 @@
     SCIA.head = document.getElementsByTagName("head")[0];
     SCIA.ZuneView = {
 
-        init: function(viewportWidth_tiles, viewportHeight_tiles, margin, zuneContainerId) {
+        init: function(viewportTiles, margin, zuneContainerId) {
 
-            var minimap = (function(viewportWidth_tiles, viewportHeight_tiles) {
+            var minimap = (function(viewportTiles) {
 
                 function nextChar(c) {
                     return String.fromCharCode(c.charCodeAt(0) + 1);
                 }
 
-                var grid = new Array(viewportWidth_tiles);
+                function ZuneElement(size, originX, originY) {
 
-                for (c = 0; c < viewportWidth_tiles; c++) {
-                    grid[c] = new Array(viewportHeight_tiles);
+                    function randomColor() {
+                        return Math.floor(Math.random() * 256) + 1;
+                    }
+
+                    var element = document.createElement("div");
+                    element.className = "element-" + size;
+                    element.style.width = tile.width * size;
+                    element.style.height = tile.height * size;
+                    element.style.top = originY;
+                    element.style.left = originX;
+                    element.style.background = "rgb(" + randomColor() + "," + randomColor() + "," + randomColor() + ")";
+
+                    return element;
                 }
 
-                var gridChar = "A";
+                var viewport = SCIA.utils.getWindowSize(),
+                    grid = new Array(viewportTiles.width),
+                    gridChar = "A",
+                    tile = {
+                        width: viewport.width / viewportTiles.width,
+                        height: viewport.width / viewportTiles.width
+                    },
+                    renderX = 0,
+                    renderY = 0,
+                    element = null,
+                    zuneContainer = document.getElementById(zuneContainerId);
+
+                for (c = 0; c < viewportTiles.width; c++) {
+                    grid[c] = new Array(viewportTiles.height);
+                }
+
 
                 // iterate over the entire grid
-                for (r = 0; r < viewportHeight_tiles; r++) {
-                    for (c = 0; c < viewportWidth_tiles; c++) {
+                for (r = 0; r < viewportTiles.height; r++) {
+                    for (c = 0; c < viewportTiles.width; c++) {
 
                         // find the first open slot in the grid
                         if (grid[c][r] !== undefined) {
-                            // something exists in this grid location, continue
+                            // something exists at this grid location, continue
                             continue;
                         }
 
@@ -43,7 +69,7 @@
                             trueSize = 0;
 
                         // find the true size of the element
-                        while (trueSize < size && gridX < viewportWidth_tiles && grid[gridX][gridY] === undefined) {
+                        while (trueSize < size && gridX < viewportTiles.width && grid[gridX][gridY] === undefined) {
                             trueSize++;
                             gridX++;
                         }
@@ -55,19 +81,25 @@
                             }
                         }
 
+                        // we know the true size of the element, as well as the start location of where to render it
+                        renderX = c * tile.width;
+                        renderY = r * tile.height;
+                        element = new ZuneElement(trueSize, renderX, renderY);
+                        zuneContainer.appendChild(element);
+
+
                         gridChar = nextChar(gridChar);
                     }
                 }
 
                 return grid;
-
-            })(viewportWidth_tiles, viewportHeight_tiles);
+            })(viewportTiles);
 
 
             // print minimap
-            for (var r = 0; r < viewportHeight_tiles; r++) {
+            for (var r = 0; r < viewportTiles.height; r++) {
                 var line = "";
-                for (var c = 0; c < viewportWidth_tiles; c++) {
+                for (var c = 0; c < viewportTiles.width; c++) {
                     line += minimap[c][r] + " ";
                 }
                 console.log(line);
