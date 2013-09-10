9facb8e87f7526f1dc8ae078f075199e825bfb5b
Nikhil Handyal <nhandyal@gmail.com>
2013-09-10 12:44:54 -0700

    minimap generation accurate

--- public/js/dist/SCIA.js
+++ public/js/dist/SCIA.js
@@ -13,53 +13,64 @@
 
         init: function(viewportWidth_tiles, viewportHeight_tiles, margin, zuneContainerId) {
 
-            var zuneElement = function(size, originX, originY) {
-                var element = document.createElement("div");
-                element.className = "element-" + size;
-                element.style.width = tile.width * size;
-                element.style.height = tile.height * size;
-                element.style.top = originY;
-                element.style.left = originX;
+            var minimap = (function(viewportWidth_tiles, viewportHeight_tiles) {
 
-                return element;
-            };
-
-            if (viewportWidth_tiles === undefined || viewportHeight_tiles === undefined || margin === undefined || zuneContainerId === undefined) {
-                throw "insufficient input arguments";
-            }
-
-            // calculate tile size
-            var viewport = SCIA.utils.getWindowSize();
-            var tile = {
-                width: viewport.width / viewportWidth_tiles,
-                height: viewport.width / viewportWidth_tiles
-            };
-            var zunePort = document.getElementById(zuneContainerId),
-                renderX = 0,
-                renderY = 0;
-
-
-            while (renderY < viewport.height) {
-                while (renderX < viewport.width) {
-                    // check if an element exists below the given coordinates
-                    console.log("(" + renderX + "," + renderY + ") : " + document.elementFromPoint(renderX, renderY));
-                    var elementBelow = document.elementFromPoint(renderX, renderY);
-                    if (elementBelow !== null && elementBelow.id != zuneContainerId) {
-                        console.log("Element below, continuing");
-                        renderX += tile.width;
-                        console.log("final loop skip: (" + renderX + "," + renderY + ")");
-                        continue;
-                    }
-
-                    // pick size = [1,4] and create an appropriately sized element
-                    var size = Math.floor(Math.random() * 4) + 1;
-                    var element = new zuneElement(size, renderX, renderY);
-                    zunePort.appendChild(element);
-                    renderX += tile.width * size;
-                    console.log("final loop : (" + renderY + "," + renderY + ")");
+                function nextChar(c) {
+                    return String.fromCharCode(c.charCodeAt(0) + 1);
                 }
-                renderY += tile.height;
-                renderX = 0;
+
+                var grid = new Array(viewportWidth_tiles);
+
+                for (c = 0; c < viewportWidth_tiles; c++) {
+                    grid[c] = new Array(viewportHeight_tiles);
+                }
+
+                var gridChar = "A";
+
+                // iterate over the entire grid
+                for (r = 0; r < viewportHeight_tiles; r++) {
+                    for (c = 0; c < viewportWidth_tiles; c++) {
+
+                        // find the first open slot in the grid
+                        if (grid[c][r] !== undefined) {
+                            // something exists in this grid location, continue
+                            continue;
+                        }
+
+                        var size = Math.floor(Math.random() * 4) + 1,
+                            gridX = c,
+                            gridY = r,
+                            trueSize = 0;
+
+                        // find the true size of the element
+                        while (trueSize < size && gridX < viewportWidth_tiles && grid[gridX][gridY] === undefined) {
+                            trueSize++;
+                            gridX++;
+                        }
+
+                        // fill the true size square on the grid
+                        for (gridX = c; gridX < c + trueSize; gridX++) {
+                            for (gridY = r; gridY < r + trueSize; gridY++) {
+                                grid[gridX][gridY] = gridChar;
+                            }
+                        }
+
+                        gridChar = nextChar(gridChar);
+                    }
+                }
+
+                return grid;
+
+            })(viewportWidth_tiles, viewportHeight_tiles);
+
+
+            // print minimap
+            for (var r = 0; r < viewportHeight_tiles; r++) {
+                var line = "";
+                for (var c = 0; c < viewportWidth_tiles; c++) {
+                    line += minimap[c][r] + " ";
+                }
+                console.log(line);
             }
         }
     };
